ca2b3cae3960c0ae774d3e2b29add5898478da1f
rajmangukiya <rajmangukiya00@gmail.com>
2022-03-05 00:36:53 +0530

    history module added

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -8,6 +8,10 @@ import "hardhat/console.sol";
 
 contract Voting {
     using Counters for Counters.Counter;
+    Counters.Counter private historyCount;
+    Counters.Counter private candidateCount;
+    // Counters.Counter private historyCount;
+
     enum EmailResponse {
         NOTAVAILABLE,
         AVAILABLE,
@@ -15,12 +19,21 @@ contract Voting {
     }
 
     mapping(string => EmailResponse) private emails;
-    mapping(string => uint256) private candidates;
+    mapping(uint256 => uint256) private candidates;
+
+    struct ElectionHistory {
+        uint256 _date;
+        mapping(uint256 => uint256) _candidates;
+    }
+    mapping(uint256 => ElectionHistory) private history;
+    mapping(uint256 => uint256) private historyCandidateCount;
 
     function addCandidate(
-        string memory candidateId
+        // string memory candidateId
     ) public {
-        candidates[candidateId] = 0;
+        uint256 newCandidateCount = candidateCount.current();
+        candidateCount.increment();
+        candidates[newCandidateCount] = 0;
     }
 
     function addEmails(
@@ -34,7 +47,7 @@ contract Voting {
 
     function voteCandidate(
         string memory email,
-        string memory candidateId
+        uint256 candidateId
     ) public {
         require(emails[email] != EmailResponse.NOTAVAILABLE, 'email should be valid to vote');
         require(emails[email] != EmailResponse. VOTED, 'email is already used to vote');
@@ -43,8 +56,36 @@ contract Voting {
     }
 
     function getResult(
-        string memory candidateId
+        uint256 candidateId
     ) public view returns(uint256) {
         return candidates[candidateId];
+    }
+
+    function closeElection() public {
+        uint256 newHistoryCount = historyCount.current();
+
+        historyCount.increment();
+        // console.log(candidateCount.current());
+
+        ElectionHistory storage newElectionHistory = history[newHistoryCount];
+        newElectionHistory._date = block.timestamp;
+        for(uint256 i = 0; i < candidateCount.current(); i++) {
+            history[newHistoryCount]._candidates[i] = candidates[i];
+        }
+
+        for(uint256 i = 0; i < candidateCount.current(); i++) {
+            candidates[i] = 0;
+        }
+        historyCandidateCount[newHistoryCount] = candidateCount.current();
+        candidateCount.reset();
+    }
+    
+    function getHistory(uint _historyCount) public view returns(uint256[] memory) {
+        uint256[] memory dumCandidates = new uint256[](historyCandidateCount[_historyCount]);
+        for(uint256 i = 0; i < historyCandidateCount[_historyCount]; i++) {
+            dumCandidates[i] = history[_historyCount]._candidates[i];
+        }
+
+        return dumCandidates;
     }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,12 +1,15 @@
 let voting
 
 describe("Voting", function () {
-  it("Should create and execute voting functionality", async function () {
+  it("should add candidates", async function () {
     const Voting = await ethers.getContractFactory("Voting")
     voting = await Voting.deploy()
+    await voting.addCandidate();
+    await voting.addCandidate();
+    await voting.addCandidate();
+  })
+  it("should add all emails", async function () {
     await voting.deployed()
-
-    // add emails
     await voting.addEmails(
       [
         '19ce001@charusat.edu.in',
@@ -14,19 +17,20 @@ describe("Voting", function () {
       ],
       2
     )
-
-    // add candidates
-    await voting.addCandidate('101');
-    await voting.addCandidate('102');
-    await voting.addCandidate('103');
-
-    // vote candidate
-    await voting.voteCandidate('19ce001@charusat.edu.in', '101');
-    await voting.voteCandidate('19ce002@charusat.edu.in', '102');
-
-    // get results
-    const result = await voting.getResult('101')
-    console.log(result);
-    
+  })
+  it("should vote to candidates", async function () {
+    await voting.voteCandidate('19ce001@charusat.edu.in', '0');
+    await voting.voteCandidate('19ce002@charusat.edu.in', '0');
+  })
+  it("should get resuls of election", async function () {
+    const result = await voting.getResult('0')
+    console.log('result', result);
+  })
+  it("should close election", async function () {
+    await voting.closeElection()
+  })
+  it("should get election history", async function () {
+    const history = await voting.getHistory(0)
+    console.log('history', history);
   })
 })
