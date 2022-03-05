4e67bc02108c892c6bf42c6845d4574b2ac94488
yagnikcreator <yagnik.creator@gmail.com>
2022-03-05 07:47:41 +0530

    Auth module design completed

---
+++ pages/auth/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import router, { useRouter } from 'next/router'
+import styles from '../../SASS/auth.module.scss'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+
+const Login = () => {
+
+
+  return (
+    <div className={styles.container}>
+      <Container className={styles.center_modal}>
+        <Row>
+          <Col className={styles.img_container}>
+            <img src='./images/auth.png' alt='background' />
+          </Col>
+          <Col className={styles.form}>
+            <div className='w-100 d-flex flex-column justify-contant-center align-items-center'> 
+              <div className='w-75'>
+                <h2>Login</h2>
+
+                <div>
+                  <div className={styles.input_group}>
+                    <p className={styles.input_lable}>Email</p>
+                    <input type='email' placeholder='Enter email' className={styles.input_field} />
+                  </div>
+
+                  <div className={styles.input_group}>
+                    <p className={styles.input_lable}>Password</p>
+                    <input type='password' placeholder='Enter password' className={styles.input_field} />
+                  </div>
+                </div>
+              </div>
+
+              <div className='w-75'>
+                <Button className={styles.login_btn} >Login</Button>
+
+                <p onClick={() => router.push('/signup')} className={styles.create_acc_txt}>Don't have an account? <span>Signup</span></p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div >
+  )
+}
+
+export default Login
